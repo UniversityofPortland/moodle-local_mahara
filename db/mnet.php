932d7721064681d58e0b42e108dd975b4ad554b7
Aaron Wells <aaronw@catalyst.net.nz>
2011-08-22 15:54:18 +1200

    Stripping out unneeded published functions

--- db/mnet.php
+++ db/mnet.php
@@ -7,9 +7,7 @@ $publishes = array(
         'classname'  => 'maharaviewsubmission_mnetservice',
         'filename'   => 'mnetlib.php',
         'methods'    => array(
-            'get_views_for_user',
-            'submit_view_for_assessment',
-            'release_submitted_view',
+            'donothing',
         ),
     ),
 );
--- mnetlib.php
+++ mnetlib.php
@@ -1,14 +1,12 @@
 <?php
 
+/**
+ * This class is just a placeholder, required because Moodle 2 won't let us register any mnet
+ * functions for subscribing unless we also register at least one for publishing.
+ * @author aaronw
+ *
+ */
 class maharaviewsubmission_mnetservice {
-    public function get_views_for_user($username, $query=null) {
-        return new stdClass();
-    }
-
-    public function submit_view_for_assessment($username, $viewid) {
-        return array();
-    }
-
-    public function release_submitted_view($viewid, $assessmentdata=array(), $username) {
+    public function donothing() {
     }
 }
