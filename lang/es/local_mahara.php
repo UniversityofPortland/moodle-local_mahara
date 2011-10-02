c138c2f02982c257f5b170e92e35d195167d990f
Iñaki Arenaza <iarenaza@mondragon.edu>
2011-10-02 18:10:30 +0200

    Add Spanish translation to the plugin
    Signed-off-by: Iñaki Arenaza <iarenaza@mondragon.edu>

---
+++ lang/es/local_mahara.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Funciones MNet extra de Mahara';
+$string['mahara_name'] = 'Módulo MNet extra de Mahara';
+$string['mahara_description'] = 'Funciones de Mahara usadas plugins Moodle de terceras partes.<br />Publicar este servicio en un sitio Moodle no tienen ningún efecto. Suscriba este servicio si desea poder usar los plugins de Mahara {$a}.<br />';
+?>
